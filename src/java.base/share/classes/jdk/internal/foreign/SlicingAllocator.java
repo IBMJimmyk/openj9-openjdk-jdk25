e7b3794b980a564a3cc9ebf3193424106c7f7979
jimmyk <jimmyk@ca.ibm.com>
2025-09-09 14:46:05 -0400

    Added segment backups
    Scaled back checking by a bit.

--- src/java.base/share/classes/jdk/internal/foreign/SlicingAllocator.java
+++ src/java.base/share/classes/jdk/internal/foreign/SlicingAllocator.java
@@ -31,6 +31,9 @@ import java.lang.foreign.SegmentAllocator;
 public final class SlicingAllocator implements SegmentAllocator {
 
     private final MemorySegment segment;
+    private final MemorySegment segmentBackup1;
+    private final MemorySegment segmentBackup2;
+    private final MemorySegment segmentBackup3;
 
     private long sp = 0L;
 
@@ -38,6 +41,9 @@ public final class SlicingAllocator implements SegmentAllocator {
 
     public SlicingAllocator(MemorySegment segment) {
         this.segment = segment;
+        this.segmentBackup1 = segment;
+        this.segmentBackup2 = segment;
+        this.segmentBackup3 = segment;
     }
 
     public long currentOffset() {
